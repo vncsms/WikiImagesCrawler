a7b0cff4c778bb1ab6b08ed12ecde327e6ac66df
vinicius <vmsabc@gmail.com>
2020-07-24 23:21:34 -0300

    Make arguments to execution
    - Create download images
    - Improve README
    - Revise the code

--- README.md
+++ README.md
@@ -8,7 +8,18 @@
   `pip install requests`
   
   `pip3 install beautifulsoup4`
+
+  or 
+
+  `pip install -r requirements.txt`
   
-- This algorithm will list in mongo all images from wikipedia from the articles you want. But if you need all articles you should executable in multiples tmux.
-- Maybe will take a few weeks to crawl all articles.
-- The `cont0.txt` file will be necessary to not repeat articles.
+
+- To execute: 
+	`python main.py 0 mongo download print`
+
+	- The argument `0` will choose the file `links0.txt`. This arguments need to be the first after main.py. If you have a file: `links1.txt` so the argument will be `1`.
+	- The argument `mongo` will save the url images in mongo. Otherwise will not save in mongo.
+	- The argument `download` will save all images inside the folder `images`. Otherwise not. Be careful what you will download, some images in wikipedia have some weirds and big names. And some articles has a lot of images.
+	- The argument `print` will print all url images. Otherwise not.
+
+- The `cont0.txt` file will be necessary to not repeat articles, is file will be generated.
--- main.py
+++ main.py
@@ -3,42 +3,62 @@ import requests
 from pymongo import MongoClient
 import sys
 
+
 def mongoConnection():
     client = MongoClient()
     db = client.imagesdb
     return db
 
-db = mongoConnection()
+
+if 'mongo' in sys.argv:
+    db = mongoConnection()
+
 
 def saveImage(name, db):
-	temp = db.images.find_one({'name': name})
-	if(temp is None):
-	    db.images.insert({'name': name, 'count': 1})
-	else:
-		db.images.update({'name': name}, {'$inc': {'count': 1}})
+    temp = db.images.find_one({'name': name})
+    if(temp is None):
+        db.images.insert({'name': name, 'count': 1})
+    else:
+        db.images.update({'name': name}, {'$inc': {'count': 1}})
+
+
+def saveImageFolder(url):
+    resp = requests.get('https:' + url)
+    file = open('images/'+url.split('/')[-1], 'wb')
+    file.write(resp.content)
+    file.close()
+
 
 def save_image_names(url):
     site = requests.get(url)
-    soup = BeautifulSoup(site.text,'html.parser')
-    for img in soup.find_all('a',attrs={"class": "image"}):
-        saveImage(img['href'],db)
+    soup = BeautifulSoup(site.text, 'html.parser')
+    for img in soup.find_all('a', attrs={"class": "image"}):
+        image = img.find('img')
+        image['src']
+        if 'mongo' in sys.argv:
+            saveImage(image['src'], db)
+        if 'print' in sys.argv:
+            print(image['src'])
+        if 'download' in sys.argv:
+            saveImageFolder(image['src'])
 
 
 def main():
     name_file = sys.argv[1]
-    links = open('links' + name_file + '.txt','r')
-    open('cont' + name_file + '.txt','a')
-    cont = open('cont' + name_file + '.txt','r')
+    links = open('links' + name_file + '.txt', 'r')
+    open('cont' + name_file + '.txt', 'a')
+    cont = open('cont' + name_file + '.txt', 'r')
 
     continua = []
-    
+
     for c in cont:
-         continua.append(c.replace('\n',''))
+        continua.append(c.replace('\n', ''))
 
     for link in links:
-        url = link.replace('\n','')
+        url = link.replace('\n', '')
         if(url not in continua):
             save_image_names(url)
-            cont = open('cont' + name_file + '.txt','a').write(url+'\n')
+            cont = open('cont' + name_file + '.txt', 'a').write(url+'\n')
+
 
 main()
